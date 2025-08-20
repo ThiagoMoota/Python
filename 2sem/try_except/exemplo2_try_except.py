758fe23a7c3aea309a757e816157a56e131f2fc1
ThiagoMoota <thiago_mota123@hotmail.com>
2025-08-20 10:25:49 -0300

    Adicionando exemplos da aula do dia 20-08-2025 usando try/except

---
+++ 2sem/try_except/exemplo2_try_except.py
@@ -0,0 +1,13 @@
+'''
+Exemplo com múltiplas exceções
+'''
+while True:
+    try:
+        n1 = int(input('Número 1: '))
+        n2 = int(input('Número 2: '))
+        result = n1/n2
+        print(f'Resultado: {result}')
+    except ZeroDivisionError:
+        print('Erro: Não é possivel dividir por ZERO')
+    except ValueError:
+        print('Erro: Caracter inválido')
